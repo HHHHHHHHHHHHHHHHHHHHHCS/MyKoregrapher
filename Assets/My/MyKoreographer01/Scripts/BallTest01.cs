da58cdf787235604a637d10bf46378f3f896f893
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-05-25 10:44:23 +0800

    小球和音乐的配合抖动

---
+++ Assets/My/MyKoreographer01/Scripts/BallTest01.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class BallTest01 : MonoBehaviour
+{
+    private Rigidbody rigi;
+    public string eventID;
+    public float jumpSpeed;
+
+    private void Awake()
+    {
+        rigi = GetComponent<Rigidbody>();
+        Koreographer.Instance.RegisterForEvents(eventID, BallJump);
+    }
+
+    private void BallJump(KoreographyEvent kevent)
+    {
+        Vector3 vel = rigi.velocity;
+        vel.y = jumpSpeed;
+        rigi.velocity = vel;
+    }
+}
